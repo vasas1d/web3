65eb9a34b5fd219909a1a9dcb22c6bc2827cddfd
Daniel Vasas <daniel.vasas1@gmail.com>
2022-06-20 17:58:16 +0200

    First commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.main')
+@section('content')
+<h1 class="display-1">Home Page</h1>
+<div>
+<p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Rerum pariatur perferendis corporis consequuntur similique reprehenderit dicta nihil in deserunt ducimus amet facilis fugiat, nesciunt modi dolore quod, animi libero natus!</p>
+<p>Ea recusandae labore dolore error. Odio, laboriosam accusamus maxime provident iusto natus sequi labore alias, iste, illo voluptatum! Pariatur repellat deserunt in praesentium vitae, illo unde doloribus placeat exercitationem error?</p>
+<p>Fugiat facilis voluptatibus labore a quia exercitationem iusto aspernatur praesentium nesciunt modi, assumenda molestiae. Culpa dolorem asperiores facilis cupiditate vel amet molestiae voluptate quasi vitae voluptas aperiam, recusandae debitis aspernatur.</p>
+<p>Sed assumenda, sapiente reiciendis blanditiis modi aliquid! Id a minus ullam quo placeat odio voluptas modi, laudantium rem autem expedita. Minima esse consectetur numquam tempora provident delectus, laboriosam facilis obcaecati?</p>
+<p>Cum hic esse quos enim commodi quibusdam, provident doloribus itaque autem. Iste est quod quae, beatae fugiat ea laboriosam ut, debitis dolor molestias ducimus delectus excepturi non in dolores accusamus.</p>
+<p>Voluptates corporis minus quas autem quia dolores aspernatur exercitationem modi sed delectus! Eveniet, numquam esse? Reprehenderit fuga totam accusamus corrupti labore aperiam. Nemo praesentium ea recusandae tempora sequi beatae quisquam.</p>
+<p>At, a quisquam. Maxime numquam nisi aliquam nobis excepturi commodi sit harum tempora, ullam placeat tenetur illo. Autem doloremque assumenda odit, eum earum asperiores. Excepturi qui temporibus ut quasi quas!</p>
+<p>Earum ducimus sit ratione quis? Odio recusandae modi, rerum corporis necessitatibus quos tempore! Tenetur esse, non velit excepturi fuga vero libero! Provident aliquam voluptates, excepturi nobis quia dolorem reprehenderit facilis?</p>
+<p>Nemo aperiam iste, harum vitae, adipisci sed consequatur eaque laboriosam recusandae tenetur omnis blanditiis reprehenderit magni possimus nesciunt, esse nam dolor explicabo? Dolorum numquam dolores rem alias, minus architecto in.</p>
+<p>Officiis aliquid quia dolores magni, officia magnam voluptatem itaque ad soluta beatae qui! Saepe, quos odit! Expedita voluptatum, cum quo quia quasi dolorum odio cumque doloremque dolor, accusantium consequatur? Velit!</p>
+</div>
+@endsection
---
+++ resources/views/layouts/main.blade.php
@@ -0,0 +1,92 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
+    <meta name="generator" content="Hugo 0.98.0">
+    <title>Blog Template · Bootstrap v5.2</title>
+
+    <link rel="canonical" href="https://getbootstrap.com/docs/5.2/examples/blog/">
+
+    
+
+    
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
+    <style>
+      .bd-placeholder-img {
+        font-size: 1.125rem;
+        text-anchor: middle;
+        -webkit-user-select: none;
+        -moz-user-select: none;
+        user-select: none;
+      }
+
+      @media (min-width: 768px) {
+        .bd-placeholder-img-lg {
+          font-size: 3.5rem;
+        }
+      }
+
+      .b-example-divider {
+        height: 3rem;
+        background-color: rgba(0, 0, 0, .1);
+        border: solid rgba(0, 0, 0, .15);
+        border-width: 1px 0;
+        box-shadow: inset 0 .5em 1.5em rgba(0, 0, 0, .1), inset 0 .125em .5em rgba(0, 0, 0, .15);
+      }
+
+      .b-example-vr {
+        flex-shrink: 0;
+        width: 1.5rem;
+        height: 100vh;
+      }
+
+      .bi {
+        vertical-align: -.125em;
+        fill: currentColor;
+      }
+
+      .nav-scroller {
+        position: relative;
+        z-index: 2;
+        height: 2.75rem;
+        overflow-y: hidden;
+      }
+
+      .nav-scroller .nav {
+        display: flex;
+        flex-wrap: nowrap;
+        padding-bottom: 1rem;
+        margin-top: -1px;
+        overflow-x: auto;
+        text-align: center;
+        white-space: nowrap;
+        -webkit-overflow-scrolling: touch;
+      }
+    </style>
+
+    
+    <!-- Custom styles for this template -->
+    <link href="https://fonts.googleapis.com/css?family=Playfair&#43;Display:700,900&amp;display=swap" rel="stylesheet">
+    <!-- Custom styles for this template -->
+    <link href={{asset('/css/blog.css')}} rel="stylesheet">
+  </head>
+  <body>
+    @include('layouts._header')
+
+
+<main class="container">
+@yield('content')
+</main>
+@include('layouts._footer')
+
+
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
+    
+  </body>
+</html>
