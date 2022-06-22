c3d6d9385b1dc47d3a61f473890fb5927c13c9ca
vasas1d <daniel.vasas1@gmail.com>
2022-06-22 21:53:36 +0200

    Registration 2.1

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -13,7 +13,7 @@
 
                     <div class="mb-3">
                         <label for="name">{{ __('Name') }}</label>
-                        <input type="text" class="form-control{{ $errors->has('name') ?   : '' }}" name="name" value="{{ old('name') }}">
+                        <input type="text" class="form-control{{ $errors->has('name') ? ' is-invalid'  : '' }}" name="name" value="{{ old('name') }}">
                         @if ($errors->has('name'))
                             <p class="invalid-feedback">
                                 {{ $errors->first('name') }}
@@ -23,7 +23,7 @@
 
                     <div class="mb-3">
                         <label for="email">{{ __('E-mail address') }}</label>
-                        <input type="email" class="form-control{{ $errors->has('email') ?   : '' }}" name="email" value="{{ old('email') }}">
+                        <input type="email" class="form-control{{ $errors->has('email') ? ' is-invalid'  : '' }}" name="email" value="{{ old('email') }}">
                         @if ($errors->has('email'))
                             <p class="invalid-feedback">
                                 {{ $errors->first('email') }}
@@ -33,7 +33,7 @@
 
                     <div class="mb-3">
                         <label for="password">{{ __('Password') }}</label>
-                        <input type="password" class="form-control{{ $errors->has('password') ?   : '' }}" name="password" value="{{ old('password') }}">
+                        <input type="password" class="form-control{{ $errors->has('password') ? ' is-invalid'  : '' }}" name="password" value="">
                         @if ($errors->has('password'))
                             <p class="invalid-feedback">
                                 {{ $errors->first('password') }}
@@ -43,18 +43,35 @@
 
                     <div class="mb-3">
                         <label for="password_confirmation">{{ __('Password confirmation') }}</label>
-                        <input type="password" class="form-control{{ $errors->has('password_confirmation') ?   : '' }}" name="password_confirmation" value="{{ old('password_confirmation') }}">
-                        @if ($errors->has('password_confirmation'))
-                            <p class="invalid-feedback">
-                                {{ $errors->first('password_confirmation') }}
-                            </p>
-                        @endif
+                        <input type="password" class="form-control{{ $errors->has('password_confirmation') }}" name="password_confirmation" value="">
                     </div>
-                    <div class="d-grid">
+
+                    <div class="mb-3">
+                        <div class="form-check">
+                            <input class="form-check-input{{ $errors->has('terms') ? ' is-invalid'  : '' }}" type="checkbox" value="1" id="terms" name="terms">
+                            <label class="form-check-label" for="terms">
+                              {{ __('Agree the terms and conditions') }}
+                            </label>
+                            @if ($errors->has('terms'))
+                                <p class="invalid-feedback">
+                                    {{ $errors->first('terms') }}
+                                </p>
+                            @endif
+                          </div>
+                    </div>
+
+                    <div class="d-grid mb-3">
                         <button class="btn btn-primary btn-lg">
                             {{ __('Sign up') }}
                         </button>
                     </div>
+
+                    <div class="d-grid">
+                        <a href="{{ route('login') }}" class="btn btn-link btn-lg">
+                            {{ __('Already have an account? Sign up here') }}
+                        </a>
+                    </div>
+
                 </form>
             </div>
         </div>
