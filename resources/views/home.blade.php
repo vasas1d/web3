6bf213d37ecda976d8abb2e3fd7dd97f0157f4d4
vasas1d <daniel.vasas1@gmail.com>
2022-06-23 00:32:15 +0200

    itemblade

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,13 +4,7 @@
 <div class="row">
     @foreach ($posts as $post)
       <div class="col-md-8 col-lg-6 mx-auto"> 
-        <div class="card mb-3">
-            <div class="card-body">
-                <h5 class="display-5">{{ $post->title }}</h5>
-                <p class="fw-bold">{{ $post->topic->name }}</p>
-                <p>{{ $post->description }}</p>
-            </div>
-        </div>
+        @include('posts._item')
       </div>
     @endforeach
 </div>
---
+++ resources/views/posts/_item.blade.php
@@ -0,0 +1,17 @@
+<div class="card mb-3">
+    <div class="card-body">
+        <h5 class="display-5">{{ $post->title }}</h5>
+        <div class="d-flex">
+            <a href="#">
+                <img class="rounded-circle" width="25" src="{{ $post->author->avatar }}" alt="{{ $post->author->name }}">
+                {{ $post->author->name }}
+            </a>
+            <span class="ms-3">{{ "Posted:" }}</span>
+            <span class="ms-3">{{ $post->created_at->diffForHumans() }}</span>
+
+            <span class="ms-3">{{ "Theme:" }}</span>
+            <span class="fw-bold ms-3">{{ $post->topic->name }}</span>
+        </div>
+        <p>{{ $post->description }}</p>
+    </div>
+</div>
