f5ddebf14d39e6364acc4dd518a6a26fbe10df3e
vasas1d <daniel.vasas1@gmail.com>
2022-06-23 03:40:08 +0200

    show.blade

--- resources/views/topics/show.blade.php
+++ resources/views/topics/show.blade.php
@@ -3,12 +3,15 @@
 @section('content')
     <h1 class="text-center mb-3">{{ $topic->name }} </h1>
     <p>{{ $topic->description }}</p>
-    @forelse ($posts as $post)
-      @include('posts._item')  
-    @empty  
-        <div class="alert alert-warning">
-            {{ 'No posts here' }}
-        </div>
-    @endforelse
-    
+    <div class="row"></div>
+    <div class="col-md-8 col-lg-6 mx-auto">
+        @forelse ($posts as $post)
+            @include('posts._item')  
+        @empty  
+            <div class="alert alert-warning">
+                {{ 'No posts here' }}
+            </div>
+        @endforelse
+    </div>
+    </div>
 @endsection
