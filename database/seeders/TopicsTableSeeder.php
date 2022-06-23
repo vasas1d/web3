a25058d6eedc806801880cbbfb1f97194127977c
vasas1d <daniel.vasas1@gmail.com>
2022-06-23 02:02:32 +0200

    SportsBlog

--- database/seeders/TopicsTableSeeder.php
+++ database/seeders/TopicsTableSeeder.php
@@ -16,17 +16,15 @@ class TopicsTableSeeder extends Seeder
      */
     public function run()
     {
-        Topic::create(['name'=>'World', 'description'=> 'Marco Rossi for President']);
-        Topic::create(['name'=>'U.S.','description'=> 'Marco Rossi for President']);
-        Topic::create(['name'=>'Technology','description'=> 'Marco Rossi for President']);
-        Topic::create(['name'=>'Design','description'=> 'Marco Rossi for President']);
-        Topic::create(['name'=>'Culture','description'=> 'Marco Rossi for President']);
-        Topic::create(['name'=>'Business','description'=> 'Marco Rossi for President']);
-        Topic::create(['name'=>'Politics','description'=> 'Marco Rossi for President']);
-        Topic::create(['name'=>'Opinion','description'=> 'Marco Rossi for President']);
-        Topic::create(['name'=>'Science','description'=> 'Marco Rossi for President']);
-        Topic::create(['name'=>'Health','description'=> 'Marco Rossi for President']);
-        Topic::create(['name'=>'Style','description'=> 'Marco Rossi for President']);
-        Topic::create(['name'=>'Travel', 'description'=> 'Marco Rossi for President']);
+        Topic::create(['name'=>'Soccer', 'description'=> 'At the top of the list of Most Popular Sports is soccer, with 3.3-3.5 billion fans all over the world.']);
+        Topic::create(['name'=>'Cricket','description'=> 'Cricket is the 2nd Most Popular Sport in the World with 2-3 billion fans (Across India, the UK, Pakistan, Asia, Australia, etc).']);
+        Topic::create(['name'=>'Basketball','description'=> 'With approximately 2-3 billion fans in the US, Canada, China, and the Philippines, Basketball is the 3rd most popular sport in the world.']);
+        Topic::create(['name'=>'Field Hockey','description'=> 'Hockey [field + Ice] is the 3rd Most Popular Sport on Earth, with 2-2.2 billion fans in Asia, Europe, Africa, Australia, etc.']);
+        Topic::create(['name'=>'Tennis','description'=> 'At No 5 on this list is Tennis. with around 1 billion fans in Europe, America, and Asia.']);
+        Topic::create(['name'=>'Volleyball','description'=> 'With around 900 million fans across Asia, Europe, the Americas, and Australia, Volleyball is the 6th Most Popular Sport in the World']);
+        Topic::create(['name'=>'Table Tennis','description'=> 'Around 850 million fans from Asia, Europe, Africa, and the Americas make Table Tennis the 7th most popular sport in the world.']);
+        Topic::create(['name'=>'Baseball','description'=> 'There are Around 500 million fans in the US, Japan, Cuba, and the Dominican Republic, making Baseball the 8th Most Popular Sport in the World.']);
+        Topic::create(['name'=>'American Football','description'=> 'The 9th most popular sport with around 390-410 million fans (France, England, New Zealand, and South Africa mainly).']);
+        Topic::create(['name'=>'Golf','description'=> 'Golf is at No 10 in popularity in the world, among other sports. With more than 390 million fans in the USA, Canada, and Europe.']);
     }
 }
--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -48,18 +48,16 @@
   
     <div class="nav-scroller py-1 mb-2">
       <nav class="nav d-flex justify-content-between">
-        <a class="p-2 link-secondary" href="#">World</a>
-        <a class="p-2 link-secondary" href="#">U.S.</a>
-        <a class="p-2 link-secondary" href="#">Technology</a>
-        <a class="p-2 link-secondary" href="#">Design</a>
-        <a class="p-2 link-secondary" href="#">Culture</a>
-        <a class="p-2 link-secondary" href="#">Business</a>
-        <a class="p-2 link-secondary" href="#">Politics</a>
-        <a class="p-2 link-secondary" href="#">Opinion</a>
-        <a class="p-2 link-secondary" href="#">Science</a>
-        <a class="p-2 link-secondary" href="#">Health</a>
-        <a class="p-2 link-secondary" href="#">Style</a>
-        <a class="p-2 link-secondary" href="#">Travel</a>
+        <a class="p-2 link-secondary" href="#">Football</a>
+        <a class="p-2 link-secondary" href="#">Cricket</a>
+        <a class="p-2 link-secondary" href="#">Basketball</a>
+        <a class="p-2 link-secondary" href="#">Field Hockey</a>
+        <a class="p-2 link-secondary" href="#">Tennis</a>
+        <a class="p-2 link-secondary" href="#">Volleyball</a>
+        <a class="p-2 link-secondary" href="#">Table Tennis</a>
+        <a class="p-2 link-secondary" href="#">Baseball</a>
+        <a class="p-2 link-secondary" href="#">American Football</a>
+        <a class="p-2 link-secondary" href="#">Golf</a>
       </nav>
     </div>
   </div>
