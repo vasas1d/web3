10c6ae3b04d0a5c1aee3b11d2f480ca963f657c7
vasas1d <daniel.vasas1@gmail.com>
2022-06-23 03:27:18 +0200

    foreachToforelse

--- resources/views/topics/show.blade.php
+++ resources/views/topics/show.blade.php
@@ -3,8 +3,12 @@
 @section('content')
     <h1 class="text-center mb-3">{{ $topic->name }} </h1>
     <p>{{ $topic->description }}</p>
-    @foreach ($posts as $post)
+    @forelse ($posts as $post)
       @include('posts._item')  
-    @endforeach
+    @empty  
+        <div class="alert alert-warning">
+            {{ 'No posts here' }}
+        </div>
+    @endforelse
     
 @endsection
